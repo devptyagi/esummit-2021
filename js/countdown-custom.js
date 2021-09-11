5ca71ab628a01187e42e398ef564ca01cb111b12
Ansh <chinrust@gmail.com>
2021-09-11 23:33:59 +0530

    title,herosection,sponsor, speaker and many more changes

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 03, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 09, 15, 18)}); // year, month, date, hour
         });
 });		
 
